49e132c389391cf1adbe50daff444c4117fc8542
bqtk0vdev <99440909+BatkovDev@users.noreply.github.com>
2024-04-16 01:20:09 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -32,13 +32,13 @@ const windows = {
 		}
 	},
 	"start-3": {
-		title: "Хуй знает что тут",
-		description: `Твое сердце остановиdadsdsadasdsaтся через <b style="color:red;">0.02 секунды</b>,
+		title: "Доступ подтвержден",
+		description: `Благодарим за содействие!
 					<br>
-					если ты выберишь <b style="color:red;">НЕТ</b>.
-					<br><br>Вы приadasdнимаете квест?`,
+					твои персоnal данные has been приняты. Access to the <b style="color: red;">Internet Protocol</b> is получен.
+					<br><br>Вы приadasdнимаете кв?ест^ Выгрузка информации`,
 		actions: {
-			accept: true,
+			accept: false,
 			deny: false
 		}
 	}
@@ -61,6 +61,9 @@ modal.innerHTML  = `<div class="border-top"></div>
 						<div class="actions">
 							<button class="accept">Да</button>
 							<button class="deny">Нет</button>
+						</div>
+						<div class="progressBar">
+							<span class="progress"></span>
 						</div>
 					</div>`;
 var modwin = {};
@@ -94,6 +97,8 @@ document.addEventListener("DOMContentLoaded", function(){
 	if(mql. matches) {
 		plot = 204;
 		console.log("Пожалуйста, поверните ориентацию экрана на ландшафтный (landscape).");
+		document.querySelector("body").innerHTML = `<h1 style="font-size:30px;color:red;text-align:center;">Пожалуйста, поверните устройство в ландшафтную ориентацию и перезагрузите страницу</h1>`;
+		return;
 	} else {
 		plot = 200;
 		console.log("Сюжет начат...)");
@@ -152,13 +157,13 @@ function nextPlot(id, hasChoice = false){
 	let delay = 2500;
 	switch(id){
 		case "prolog":
-			delay = 1800;
+			delay = 2200;
 			return setTimeout(function(){
 				modals.innerHTML = "";
 				chNotification("start-1", windows["start-1"]);
 			}, delay);
 		case "start-1":
-			delay = 1500;
+			delay = 2000;
 			return setTimeout(function(){
 				modals.innerHTML = "";
 				chNotification("start-2", windows["start-2"]);
@@ -171,9 +176,20 @@ function nextPlot(id, hasChoice = false){
 				})
 				modwin.actions.deny.addEventListener("click", function(e){
 					e.preventDefault();
-					console.log("Раз отказался - динаху отсюда.");
+					window.location.href = "https://accounts.google.com/v3/signin/identifier?continue=https%3A%2F%2Ftakeout.google.com%2F%3Fhl%3Dru&followup=https%3A%2F%2Ftakeout.google.com%2F%3Fhl%3Dru&hl=ru&ifkv=ARZ0qKInjuen74k3UNCZHJrRYPKwbdLUN_BY8UG04kc7QLYbEZgIs5Eo-P1aN15M3sDs-11rAEk9cg&osid=1&passive=1209600&flowName=GlifWebSignIn&flowEntry=ServiceLogin&dsh=S755098695%3A1713210490600145&theme=mn&ddm=0";
 				})
 			}
 			break;
+			case "start-3":
+				delay = 31000;
+				return setTimeout(function(){
+					document.querySelector("body").innerHTML = `<div class="intro">
+					<div class="video">
+						<video class="vmedia" id="video-1" src="stock.webm" muted autoplay loop></video>
+					</div>
+				</div>`;
+				}, delay);
+				
+				break;
 	}
 }
